b973a632e096e4577d16a7ceb4b93c1ae4b2d767
Ben Novikov <bigopendatanet@yahoo.com>
2018-02-16 16:49:42 +0200

    Implement simplest inMemoryAuthentication

--- src/main/java/io/github/benkoff/springsecurity/Configuration/SecurityConfig.java
+++ src/main/java/io/github/benkoff/springsecurity/Configuration/SecurityConfig.java
@@ -3,6 +3,7 @@ package io.github.benkoff.springsecurity.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -14,6 +15,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter  {
     @Autowired
     public void configureGlobal (AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+        auth.inMemoryAuthentication().withUser("user").password("{noop}password").roles("USER");
+    }
+
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests().anyRequest().authenticated().and()
+                .formLogin().and()
+                .httpBasic();
     }
 }
---
+++ src/main/java/io/github/benkoff/springsecurity/Configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,9 @@
+package io.github.benkoff.springsecurity.Configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by Ben Novikov on 2018, February, 16
+ */
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
