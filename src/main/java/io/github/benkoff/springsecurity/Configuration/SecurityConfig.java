0c55556ba74f04f6c7801438c87b0d37dfd2f208
Ben Novikov <bigopendatanet@yahoo.com>
2018-02-16 15:10:52 +0200

    Initial commit with SpringBoot 2.0 and Basic Configuration

---
+++ src/main/java/io/github/benkoff/springsecurity/Configuration/SecurityConfig.java
@@ -0,0 +1,19 @@
+package io.github.benkoff.springsecurity.Configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by Ben Novikov on 2018, February, 16
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter  {
+    @Autowired
+    public void configureGlobal (AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+    }
+}
